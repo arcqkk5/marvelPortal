7bfefdbfc5f24e029ec6f53f648af766af111d4b
arcqkk5 <arcq-k5@yandex.ru>
2022-02-02 17:48:21 +0300

    add skeleton and charinfo component

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,5 +1,6 @@
 import "./charList.scss";
 import { Component } from "react/cjs/react.production.min";
+import PropTypes from "prop-types";
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
@@ -13,19 +14,44 @@ class CharList extends Component {
     charList: [],
     loading: true,
     error: false,
+    newItemLoading: false,
+    offset: 210,
+    charEnded: false,
   };
 
   marvelService = new MarvelService();
 
   componentDidMount() {
-    this.marvelService
-      .getAllCharacters()
-      .then(this.onCharListLoaded)
-      .catch(this.onListError);
+    this.onRequest();
   }
 
-  onCharListLoaded = (charList) => {
-    this.setState({ charList, loading: false });
+  onRequest = (offset) => {
+    this.onCharListLoading();
+    this.marvelService
+      .getAllCharacters(offset)
+      .then(this.onCharListLoaded)
+      .catch(this.onListError);
+  };
+
+  onCharListLoading = () => {
+    this.setState({
+      newItemLoading: true,
+    });
+  };
+
+  onCharListLoaded = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+
+    this.setState(({ offset, charList }) => ({
+      charList: [...charList, ...newCharList],
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9,
+      charEnded: ended,
+    }));
   };
 
   onListError = () => {
@@ -54,7 +80,8 @@ class CharList extends Component {
   }
 
   render() {
-    const { charList, loading, error } = this.state;
+    const { charList, loading, error, newItemLoading, offset, charEnded } =
+      this.state;
     const items = this.renderItemCharList(charList);
     const errorMessage = error ? <ErrorMessage /> : null;
     const spinner = loading ? <Spinner /> : null;
@@ -67,7 +94,12 @@ class CharList extends Component {
           {spinner}
           {content}
         </ul>
-        <button className="button button__main button__long">
+        <button
+          className="button button__main button__long"
+          disabled={newItemLoading}
+          onClick={() => this.onRequest(offset)}
+          style={{ display: charEnded ? "none" : "block" }}
+        >
           <div className="inner">load more</div>
         </button>
       </div>
@@ -75,4 +107,7 @@ class CharList extends Component {
   }
 }
 
+CharList.propTypes = {
+  onCharSelected: PropTypes.func,
+};
 export default CharList;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,8 @@ class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
   _apiKey = "apikey=5244d0b27c6a8457f1fd5729442bc99f";
 
+  _baseOffset = 210;
+
   getResource = async (url) => {
     let res = await fetch(url);
 
@@ -12,9 +14,9 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = async () => {
+  getAllCharacters = async (offset = this._baseOffset) => {
     const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
     );
 
     return res.data.results.map(this._transformCharacter);
