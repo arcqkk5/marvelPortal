7b47951cf1ef219592e4670fd0b666872a2c3875
arcqkk5 <arcq-k5@yandex.ru>
2022-02-02 14:07:34 +0300

    change charnfo

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -40,7 +40,11 @@ class CharList extends Component {
       }
 
       return (
-        <li className="char__item" key={item.id}>
+        <li
+          className="char__item"
+          key={item.id}
+          onClick={() => this.props.onCharSelected(item.id)}
+        >
           <img src={item.thumbnail} alt={item.name} style={classNamesImg} />
           <div className="char__name">{item.name}</div>
         </li>
